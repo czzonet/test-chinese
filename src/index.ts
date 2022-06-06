93b3a67cae5363bfddb8868311fc7ccf174800e6
czzonet <czzonet@163.com>
2022-06-06 09:27:31 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 console.log(0);
+
+const 变量 = 0;
+const 获取变量 = () => {
+  return 变量;
+};
+
+const 结果 = 获取变量();
+console.log(结果);
+
+const 求和 = (a: number, b: number) => a + b;
+
+console.log(求和(8, 9));
